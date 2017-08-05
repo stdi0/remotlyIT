54a0f6193533013376f7bd5bc4d86a7cab6f15c3
Denis <denisyurievich94@gmail.com>
2017-08-05 11:03:26 +0300

    bot

--- main.go
+++ main.go
@@ -178,7 +178,7 @@ func main() {
 			if err = db.QueryRow("INSERT INTO Jobs (publish_date, title, description, section) VALUES ($1, $2, $3, $4) RETURNING id", time.Now(), r.Form["title"][0], r.Form["description"][0], r.Form["section"][0]).Scan(&lastID); err != nil {
 					log.Println(err)
 			}
-			if _, err = db.Exec("INSERT INTO Tags (job, tag) VALUES ($1, $2)", lastID, r.Form["tags"][0]); err != nil {
+			if _, err = db.Exec("INSERT INTO Tags (job_id, tag) VALUES ($1, $2)", lastID, r.Form["tags"][0]); err != nil {
 					log.Println(err)
 			}
 		}
