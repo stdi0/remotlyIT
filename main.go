16e58ae1d636e8ec399c7b658473612acfba4da5
Denis <denisyurievich94@gmail.com>
2017-08-06 17:30:10 +0300

    bot

--- main.go
+++ main.go
@@ -311,7 +311,7 @@ func main() {
 				pointer = "Программисты"
 				count = tagCountSend("c/c++", update.Message.Chat.Id, count, "C/CC%2B%2B (ещё)")
 			case "C#": 
-				sendMessage(update.Message.Chat.Id, "Это сообщение отобразится при отправке /start", "")
+				sendMessage(update.Message.Chat.Id, "C#", "")
 				pointer = "Программисты"
 				count = tagSend("c#", update.Message.Chat.Id, "C# (ещё)")
 			case "C# (ещё)":
